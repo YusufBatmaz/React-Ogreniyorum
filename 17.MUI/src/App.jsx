c1c0d3362aad88155063a22e840436731f451057
Yusuf Batmaz <batmazh358@gmail.com>
2025-08-01 16:03:38 +0300

    Component Kütüphaneleri ve Çağrıları

--- 17.MUI/src/App.jsx
+++ 17.MUI/src/App.jsx
@@ -2,9 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 import MUIButton from './components/MUIButton'
 import MUITextField from './components/MUITextField'
 import MUISelect from './components/MUISelect'
+import MUICheckbox from './components/MUICheckbox'
+import MUIAutoComplete from './components/MUIAutoComplete'
+import MUIBox from './components/MUIBox'
+import MUIStack from './components/MUIStack'
+import MUIGrid from './components/MUIGrid'
+import MUICard from './components/MUICard'
+import MUIAccirdion from './components/MUIAccirdion'
+import MUIAppbar from './components/MUIAppbar'
+import MUIImageList from './components/MUIImageList'
+import MUIDrawer from './components/MUIDrawer'
+import MUIBadge from './components/MUIBadge'
+import MUIAvatar from './components/MUIAvatar'
+import MUITooltip from './components/MUITooltip'
+import MUIAlert from './components/MUIAlert'
+import MUIDialog from './components/MUIDialog'
+import MUISnackbar from './components/MUISnackbar'
+import MUIProgress from './components/MUIProgress'
+import MUISkeleton from './components/MUISkeleton'
+import MUITable from './components/MUITable'
+import MUITabs from './components/MUITabs'
 
 function App() {
 
@@ -12,7 +33,27 @@ function App() {
     <div>
       {/* <MUIButton /> */}
       {/* <MUITextField /> */}
-      <MUISelect />
+      {/* <MUISelect /> */}
+      {/* <MUICheckbox /> */}
+      {/* <MUIAutoComplete /> */}
+      {/* <MUIBox /> */}
+      {/* <MUIStack /> */}
+      {/* <MUIGrid /> */}
+      {/* <MUICard /> */}
+      {/* <MUIAccirdion /> */}
+      {/* <MUIAppbar /> */}
+      {/* <MUIImageList /> */}
+      {/* <MUIDrawer /> */}
+      {/* <MUIBadge /> */}
+      {/* <MUIAvatar /> */}
+      {/* <MUITooltip /> */}
+      {/* <MUIAlert /> */}
+      {/* <MUIDialog /> */}
+      {/* <MUISnackbar /> */}
+      {/* <MUIProgress /> */}
+      {/* <MUISkeleton /> */}
+      {/* <MUITable /> */}
+      <MUITabs />
     </div>
   )
 }
