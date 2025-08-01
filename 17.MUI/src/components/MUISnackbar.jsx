b85f808a43380c20b52c8bb952ac9ac7ae68c29e
Yusuf Batmaz <batmazh358@gmail.com>
2025-08-01 16:09:52 +0300

    MUI ile Snackbar

---
+++ 17.MUI/src/components/MUISnackbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
+
+function MUISnackbar() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    const action = (
+        <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+                UNDO
+            </Button>
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+            >
+                <CloseIcon fontSize="small" />
+            </IconButton>
+        </React.Fragment>
+    );
+
+    return (
+        <div>
+            <Button onClick={handleClick}>Snackbar Aç</Button>
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                message="Snackbar açıldı."
+                action={action}
+            />
+        </div>
+    );
+}
+
+export default MUISnackbar
