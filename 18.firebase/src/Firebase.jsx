29e2b022a4e3788381fd2a3b926c813c23f17e12
Yusuf Batmaz <128752354+YusufBatmaz@users.noreply.github.com>
2025-08-04 14:28:51 +0300

    Update Firebase.jsx

--- 18.firebase/src/Firebase.jsx
+++ 18.firebase/src/Firebase.jsx
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD88UL73SFdMtbodCrYYdBGj0wzK-yBaZM",
-  authDomain: "first-firebase-59f08.firebaseapp.com",
-  projectId: "first-firebase-59f08",
-  storageBucket: "first-firebase-59f08.firebasestorage.app",
-  messagingSenderId: "564095232170",
-  appId: "1:564095232170:web:9dfe4053284a74668c88d3"
+  //Your firebase informations
 };
 
 export const app = initializeApp(firebaseConfig);
